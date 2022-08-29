0392bdf580c58968eb0a89892092d840b4def3e7
Florencia Díaz de la Fuente <dflorencia07@gmail.com>
2022-08-29 11:47:40 -0300

    desafío menú

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 
 import './App.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
 
-let imagen = "cuartoDeLibra"
-let titulo = "Doble Cuarto de Libra con Queso"
-let calorias = "771 kcal"
-let descripcion = "Imaginá la sensación del clásico Cuarto de Libra. Imaginalo con un medallón de deliciosa carne 100% vacuna, queso cheddar, cebolla, kétchup y mostaza ¿Listo? Ahora duplicá esa sensación. Ya tenés el Doble Cuarto en la cabeza."
+
 
   return (
-    <div className='container'>
-      <div className="row">
-        <div className='col-md-4 offset-md-2'>
-        <img src={imagen} alt={titulo} className="img-fluid" />
-        </div>
-        <div className='col-md-4'>
-        <h1>{titulo}</h1>
-        <p>{calorias}</p>
-        <p>{descripcion}</p>
-        </div>
-      </div>
+    <div>
+      <NavBar />
+      <hr></hr>
+      <h2>Bikes Store</h2>
+      <hr></hr>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from "./assets/images/bicicleta.png"
+
+const NavBar = () => {
+    // let estilo = {
+    //     color: "white",
+    //     backgroundColor: "black"}
+    
+    
+        return (
+            <div className="container">
+            <ul className="nav d-flex align-items-center">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#"><img src={logo} width="48" alt="logo bici" /></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link link_header" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link link_header" href="#">Tienda</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link link_header" href="#">Nosotros</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link link_header" href="#">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    
+        )
+    
+    
+    }
+    
+    
+    
+
+export default NavBar;
